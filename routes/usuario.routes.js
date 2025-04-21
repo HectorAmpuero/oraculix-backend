641aa0bfffe12c3150babaaac29d440ac068e8a4
Gonzalo <h.munozampuero@gmail.com>
2025-04-21 19:05:49 -0400

    fixss

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -5,6 +5,11 @@ const usuarioController = require("../controllers/usuario.controller");
 // Ruta para registrar usuario
 router.post("/registrar", usuarioController.registrarUsuario);
 
+router.get("/test", (req, res) => {
+    res.send("Ruta /api/usuarios/test funcionando");
+  });
+  
+
 // Ruta para login
 router.post("/login", usuarioController.loginUsuario);
 
