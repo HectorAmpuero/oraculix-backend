afd3eb7a1db875c4c8998457d0d2ec5b7f3a71f2
Gonzalo <h.munozampuero@gmail.com>
2025-04-21 21:09:04 -0400

    final database_url

--- config/db.js
+++ config/db.js
@@ -1,13 +1,8 @@
-// config/db.js
 require("dotenv").config();
 const { Pool } = require("pg");
 
 const db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
